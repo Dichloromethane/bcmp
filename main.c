79cf623fdea1e66599e3024ac4a9cf57e1a35f42
Your Name <you@example.com>
2019-06-05 22:53:35 +0200

    Made it a bit faster

--- main.c
+++ main.c
@@ -9,12 +9,13 @@ char lng[] = "VeryVeryLongByteThingDUUDUDUDUDUDUDU";
 char one_diff[] = "VeryVeryLongByteThingDUUDUDUDUDUDUDX";
 
 
-#define FORM_STR ("\"%s\" to \"%s\": %d\n")
+#define FORM_STR ("\"%s\" to \"%s\": %d, should be %d\n")
 #define MIN(a1, a2) (a1 < a2 ? a1 : a2)
-#define BCMP_TEST(a1, a2)  printf(FORM_STR, a1, a2, \
-						   bcmp_custom(a1, a2, MIN(sizeof(a1), sizeof(a2))))
-#define BCMP_TEST_L(a1, a2, l)  printf(FORM_STR, a1, a2, \
-						        bcmp_custom(a1, a2, l))
+#define BCMP_TEST(a1, a2, res)  printf(FORM_STR, a1, a2, \
+						   bcmp_custom(a1, a2, MIN(sizeof(a1), sizeof(a2))),\
+						   res)
+#define BCMP_TEST_L(a1, a2, l, res)  printf(FORM_STR, a1, a2, \
+						        bcmp_custom(a1, a2, l), res)
 
 #define N 0x10000
 
@@ -25,13 +26,13 @@ main(int argc, char **argv) {
   c2 = calloc(N, 1);
   c3 = calloc(N, 1);
   c3[N-1] = 'A';
-  BCMP_TEST(foo, lng);
-  BCMP_TEST(foo, foo);
-  BCMP_TEST(lng, lng);
-  BCMP_TEST(lng, one_diff);
-  BCMP_TEST("Foo", "");
-  BCMP_TEST("", "");
-  BCMP_TEST_L(c, c2, N);
-  BCMP_TEST_L(c, c3, N);
+  BCMP_TEST(foo, lng, 0);
+  BCMP_TEST(foo, foo, 1);
+  BCMP_TEST(lng, lng, 1);
+  BCMP_TEST(lng, one_diff, 0);
+  BCMP_TEST("Foo", "", 0);
+  BCMP_TEST("", "", 1);
+  BCMP_TEST_L(c, c2, N, 1);
+  BCMP_TEST_L(c, c3, N, 0);
   return 0;
 }
